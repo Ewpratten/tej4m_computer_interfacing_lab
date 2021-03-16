9c7aa61eb3fd38a01bedb1e60838e8c1d10f6997
Evan Pratten <ewpratten@gmail.com>
2021-03-16 12:24:00 -0400

    init serial

--- arduino/src/main.cc
+++ arduino/src/main.cc
@@ -3,6 +3,16 @@
 
 void setup()
 {
+
+    // Init the serial connection
+    Serial.begin(9600);
+
+    // Init IO
+
+    // Wait for a valid host connection
+    while (true)
+    {
+    }
 }
 
 void loop()
